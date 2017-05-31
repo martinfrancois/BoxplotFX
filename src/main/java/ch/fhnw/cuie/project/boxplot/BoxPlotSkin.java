83e56b4252de0ad68bae8d75a4b68e4a1c76e6e8
Marco Sanfratello <marco.sanfratello@students.fhnw.ch>
2017-05-31 10:56:17 +0200

    Basic functionality (should work)

--- src/main/java/ch/fhnw/cuie/project/boxplot/BoxPlotSkin.java
+++ src/main/java/ch/fhnw/cuie/project/boxplot/BoxPlotSkin.java
@@ -26,7 +26,6 @@ public class BoxPlotSkin<T> extends SkinBase<BoxPlotControl> {
     private static final double MINIMUM_WIDTH = 25;
     private static final double MINIMUM_HEIGHT = MINIMUM_WIDTH / ASPECT_RATIO;
     private static final double MAXIMUM_WIDTH = 800;
-public class BoxPlotSkin<T> extends SkinBase<BoxPlotControl> {
 
     private StackPane drawingPane;
 
@@ -47,7 +46,6 @@ public class BoxPlotSkin<T> extends SkinBase<BoxPlotControl> {
     private Rectangle quartiles;
     private Line lowerWhiskerLine;
     private Line upperWhiskerLine;
-    private Circle outliers;
     private Line medianLine;
 
     // ------ Variables ---------------------------------
@@ -63,31 +61,29 @@ public class BoxPlotSkin<T> extends SkinBase<BoxPlotControl> {
         setupAnimations();
         setupEventHandlers();
         //setupValueChangedListeners();
-        //setupBindings();
+        setupBindings();
         boxPlot = getSkinnable().getBoxPlot();
         outliers = boxPlot.getOutliers();
         initOutliers();
         setupValueChangeListeners();
     }
 
-    private void drawOutlier(T element, double value){
+    private void drawOutlier(T element, double value) {
         // do some magic to create the outlier and attach listener to setOnAction to change currently selected object
         Circle outlier = new Circle();
         circles.put(element, outlier);
     }
 
-    private void removeOutlier(T element){
+    private void removeOutlier(T element) {
         // remove the outlier associated with this element
         drawingPane.getChildren().remove(circles.get(element));
     }
 
-    private void initOutliers(){
+    private void initOutliers() {
         outliers.entrySet().stream()
                 .forEach(entry -> {
                     drawOutlier(entry.getKey(), entry.getValue());
                 });
-        setupBindings();
-        setupListeners();
     }
 
     private void setupBindings() {
@@ -128,21 +124,21 @@ public class BoxPlotSkin<T> extends SkinBase<BoxPlotControl> {
 //    line id="Median" class="st3" x1="1257.8" y1="331.4" x2="1257.8" y2="579.1"
 
         double widthFactor = getWidthFactor();
-        range = new Line(0,height/2, width,height/2);
-        quartiles = new Rectangle(lowerQuartil.get()*widthFactor,0,(upperQuartil.get() - lowerQuartil.get())*widthFactor, height);
-        lowerWhiskerLine = new Line(lowerWhisker.get()*widthFactor, 0, lowerWhisker.get()*widthFactor, height);
-        upperWhiskerLine = new Line(upperWhisker.get()*widthFactor, 0, upperWhisker.get()*widthFactor, height);
+        range = new Line(0, height / 2, width, height / 2);
+        quartiles = new Rectangle(lowerQuartil.get() * widthFactor, 0, (upperQuartil.get() - lowerQuartil.get()) * widthFactor, height);
+        lowerWhiskerLine = new Line(lowerWhisker.get() * widthFactor, 0, lowerWhisker.get() * widthFactor, height);
+        upperWhiskerLine = new Line(upperWhisker.get() * widthFactor, 0, upperWhisker.get() * widthFactor, height);
 //        outliers = new Circle(5, -15, 5);
-        medianLine = new Line(median.get()*widthFactor, 0, median.get()*widthFactor, height);
+        medianLine = new Line(median.get() * widthFactor, 0, median.get() * widthFactor, height);
     }
 
-//    Returns the factor, which is needed to resize the data
+    //    Returns the factor, which is needed to resize the data
     private double getWidthFactor() {
-        return width/(maxElement.get()-minElement.get());
+        return width / (maxElement.get() - minElement.get());
     }
 
     private void layoutParts() {
-        drawingPane.getChildren().addAll(range, quartiles, lowerWhiskerLine, upperWhiskerLine, outliers, medianLine);
+        drawingPane.getChildren().addAll(range, quartiles, lowerWhiskerLine, upperWhiskerLine, medianLine);
         getChildren().add(drawingPane);
     }
 
@@ -156,7 +152,7 @@ public class BoxPlotSkin<T> extends SkinBase<BoxPlotControl> {
 
     private void setupValueChangeListeners() {
         outliers.addListener((MapChangeListener<? super T, ? super Double>) change -> {
-            if(change.wasRemoved()) {
+            if (change.wasRemoved()) {
                 removeOutlier(change.getKey());
             } else if (change.wasAdded()) {
                 drawOutlier(change.getKey(), change.getValueAdded());
