0f8ef60545e2d0274ea018b3a1b022a2fc868991
Marco Sanfratello <marco.sanfratello@students.fhnw.ch>
2017-05-31 10:47:01 +0200

    Basic functionality should work

--- src/main/java/ch/fhnw/cuie/project/boxplot/BoxPlotSkin.java
+++ src/main/java/ch/fhnw/cuie/project/boxplot/BoxPlotSkin.java
@@ -1,16 +1,11 @@
 package ch.fhnw.cuie.project.boxplot;
 
 import javafx.beans.property.*;
-import javafx.geometry.Pos;
-import javafx.scene.control.Control;
 import javafx.scene.control.SkinBase;
-import javafx.scene.layout.Pane;
 import javafx.scene.layout.StackPane;
 import javafx.scene.shape.Circle;
 import javafx.scene.shape.Line;
 import javafx.scene.shape.Rectangle;
-
-import java.util.Arrays;
 
 /**
  * @author Dieter Holz
@@ -36,8 +31,13 @@ public class BoxPlotSkin extends SkinBase<BoxPlotControl> {
     private Rectangle quartiles;
     private Line lowerWhiskerLine;
     private Line upperWhiskerLine;
-    private Circle ausreisser;
+    private Circle outliers;
     private Line medianLine;
+
+    // ------ Variables ---------------------------------
+    private double width;
+    private double height;
+    private double offset;
 
     public BoxPlotSkin(BoxPlotControl control) {
         super(control);
@@ -46,19 +46,27 @@ public class BoxPlotSkin extends SkinBase<BoxPlotControl> {
         layoutParts();
         setupAnimations();
         setupEventHandlers();
-        setupValueChangedListeners();
         setupBindings();
+        setupListeners();
+    }
+
+    private void setupBindings() {
+
     }
 
     private void initializeSelf() {
         // ----- Initialize Properties ----------------------
-        lowerWhisker.set(-13);
-        upperWhisker.set(4);
-        median.set(-2);
-        lowerQuartil.set(-7);
-        upperQuartil.set(1);
         minElement.set(-15);
-        maxElement.set(5);
+        //    Computes the offset, from 0 to the minElement. This is needed for scaling
+        offset = minElement.get() * -1;
+
+        lowerWhisker.set(-13 + offset);
+        upperWhisker.set(4 + offset);
+        median.set(-2 + offset);
+        lowerQuartil.set(-7 + offset);
+        upperQuartil.set(1 + offset);
+        maxElement.set(5 + offset);
+        minElement.set(0);
 
         // ----- CSS ----------------------------------------
         String fonts = getClass().getResource(FONTS_CSS).toExternalForm();
@@ -69,45 +77,55 @@ public class BoxPlotSkin extends SkinBase<BoxPlotControl> {
     }
 
     private void initializeParts() {
+        drawingPane = new StackPane();
+        drawingPane.getStyleClass().add("drawingPane");
+
         //    line id="range" class="st1" x1="325.6" y1="455.7" x2="1798.4" y2="455.7"
 //    rect id="quartiles" x="831.7" y="331.4" class="st2" width="673.7" height="247.7"
 //    line id="lowerWhisker" class="st1" x1="325.6" y1="366.1" x2="325.6" y2="539.8"
 //    line id="upperWhisker" class="st1" x1="1798.4" y1="366.1" x2="1798.4" y2="539.8"
 //    circle id="Ausreisser" class="st1" cx="199.3" cy="453" r="14"
 //    line id="Median" class="st3" x1="1257.8" y1="331.4" x2="1257.8" y2="579.1"
-        double width = this.widthProperty().getWitdh();
-        double height = this.heightProperty().getHeight();
 
-        range = new Line(5,height/2, width-5,height/2);
-        quartiles = new Rectangle(15, -20, 20, 20);
-        lowerWhiskerLine = new Line(10, -5, 10, -15);
-        upperWhiskerLine = new Line(40, -5, 40, -15);
-        ausreisser = new Circle(5, -15, 5);
-        medianLine = new Line(30, -5, 30, -15);
+        double widthFactor = getWidthFactor();
+        range = new Line(0,height/2, width,height/2);
+        quartiles = new Rectangle(lowerQuartil.get()*widthFactor,0,(upperQuartil.get() - lowerQuartil.get())*widthFactor, height);
+        lowerWhiskerLine = new Line(lowerWhisker.get()*widthFactor, 0, lowerWhisker.get()*widthFactor, height);
+        upperWhiskerLine = new Line(upperWhisker.get()*widthFactor, 0, upperWhisker.get()*widthFactor, height);
+//        outliers = new Circle(5, -15, 5);
+        medianLine = new Line(median.get()*widthFactor, 0, median.get()*widthFactor, height);
+    }
 
-        // always needed
-        drawingPane = new StackPane();
-        drawingPane.getStyleClass().add("drawingPane");
-        drawingPane.setMaxSize(ARTBOARD_WIDTH, ARTBOARD_HEIGHT);
-        drawingPane.setMinSize(ARTBOARD_WIDTH, ARTBOARD_HEIGHT);
-        drawingPane.setPrefSize(ARTBOARD_WIDTH, ARTBOARD_HEIGHT);
+//    Returns the factor, which is needed to resize the data
+    private double getWidthFactor() {
+        return width/(maxElement.get()-minElement.get());
     }
 
     private void layoutParts() {
-
-        drawingPane.getChildren().addAll(range, quartiles, lowerWhiskerLine, upperWhiskerLine, ausreisser, medianLine);
+        drawingPane.getChildren().addAll(range, quartiles, lowerWhiskerLine, upperWhiskerLine, outliers, medianLine);
         getChildren().add(drawingPane);
+    }
+
+    private void setupAnimations() {
+
     }
 
     private void setupEventHandlers() {
 
     }
 
-    private void setupValueChangeListeners() {
+    private void setupBinding() {
 
     }
 
-    private void setupBinding() {
+    private void setupListeners() {
+        drawingPane.widthProperty().addListener(e -> {
+            width = drawingPane.getWidth();
+        });
+
+        drawingPane.heightProperty().addListener(e -> {
+            height = drawingPane.getHeight();
+        });
 
     }
 
@@ -195,4 +213,5 @@ public class BoxPlotSkin extends SkinBase<BoxPlotControl> {
     public static void setMaxElement(double maxElement) {
         BoxPlotSkin.maxElement.set(maxElement);
     }
+
 }
