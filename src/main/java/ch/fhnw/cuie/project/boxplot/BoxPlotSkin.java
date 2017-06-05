e34dbffba762cf14f5d995aade40924a3cdb66ec
martinfrancois <fracoisma@hotmail.com>
2017-06-05 18:33:43 +0200

    refactored everything besides circles

--- src/main/java/ch/fhnw/cuie/project/boxplot/BoxPlotSkin.java
+++ src/main/java/ch/fhnw/cuie/project/boxplot/BoxPlotSkin.java
@@ -1,7 +1,7 @@
 package ch.fhnw.cuie.project.boxplot;
 
-import javafx.beans.InvalidationListener;
 import javafx.beans.binding.Bindings;
+import javafx.beans.binding.DoubleBinding;
 import javafx.beans.property.DoubleProperty;
 import javafx.beans.property.ReadOnlyDoubleProperty;
 import javafx.beans.property.SimpleDoubleProperty;
@@ -16,6 +16,7 @@ import javafx.scene.shape.Line;
 import javafx.scene.shape.Rectangle;
 
 import java.util.HashMap;
+import java.util.function.Function;
 import java.util.function.UnaryOperator;
 
 /**
@@ -23,6 +24,7 @@ import java.util.function.UnaryOperator;
  */
 public class BoxPlotSkin<T> extends SkinBase<BoxPlotControl> {
     private static final String DECIMALS = "0";
+    public static final String LABEL_FORMATTING = "%." + DECIMALS + "f";
     private static final double STROKE_WIDTH = 3;
     private final ObservableMap<T, Double> outliers;
     private final BoxPlot<T> boxPlot;
@@ -63,9 +65,19 @@ public class BoxPlotSkin<T> extends SkinBase<BoxPlotControl> {
     public static final double TRANSLATE_Y = 20;
     public static final double FACTOR_BOXPLOT_START = 0d;
     public static final double FACTOR_BOXPLOT_END = 0.75d;
-    public static final double FACTOR_SCALE_END = 1d;
+    public static final double FACTOR_SCALE_END = 0.9d;
+    public static final double FACTOR_DATA_SCALE_END = 1d;
+
+
     private final UnaryOperator<Double> scaleWidth = value -> (value - getOffset()) * getWidthFactor();
+    private final Function<ReadOnlyDoubleProperty, DoubleBinding> scaleWidthBinding = value -> Bindings.createDoubleBinding(
+            () -> scaleWidth.apply(value.get()), offset, widthFactor, value
+    );
+
     private final UnaryOperator<Double> scaleHeight = factor -> (height.get() * factor);
+    private final Function<Double, DoubleBinding> scaleHeightBinding = factor -> Bindings.createDoubleBinding(
+            () -> scaleHeight.apply(factor), height
+    );
 
     public BoxPlotSkin(BoxPlotControl control) {
         super(control);
@@ -177,60 +189,13 @@ public class BoxPlotSkin<T> extends SkinBase<BoxPlotControl> {
                 drawOutlier(change.getKey(), change.getValueAdded());
             }
         });
-
-        boxPlot.getData().addListener((InvalidationListener) observable -> {
-            adapt();
-        });
-
-        drawingPane.heightProperty().addListener(observable -> {
-            adapt();
-        });
-
-        drawingPane.widthProperty().addListener(observable -> {
-            adapt();
-        });
-    }
-
-    //    Draws the BoxPlot
-    private void adapt() {
-
-        range.startXProperty().set(scaleWidth.apply(boxPlot.getLowerWhisker()));
-        range.startYProperty().set((height.get() - TRANSLATE_Y) / 2);
-        range.endXProperty().set((boxPlot.getUpperWhisker() - offset.get()) * widthFactor.get());
-        range.endYProperty().set((height.get() - TRANSLATE_Y) / 2);
-
-        quartiles.xProperty().set((boxPlot.getQ1() - offset.get()) * widthFactor.get());
-        quartiles.yProperty().set(0);
-        quartiles.widthProperty().set((boxPlot.getQ3() - boxPlot.getQ1()) * widthFactor.get());
-        quartiles.heightProperty().set(height.get() - TRANSLATE_Y);
-
-        setupVerticalLineBindings(lowerWhiskerLine, boxPlot.lowerWhiskerProperty());
-        setupVerticalLineBindings(upperWhiskerLine, boxPlot.upperWhiskerProperty());
-        setupVerticalLineBindings(medianLine, boxPlot.medianProperty());
-//        setupVerticalLineBindings(currentObjectLine, currentObject, height);
     }
 
     private void setupVerticalLineBindings(Line line, ReadOnlyDoubleProperty value) {
-        line.startXProperty().bind(
-                Bindings.createDoubleBinding(
-                        () -> scaleWidth.apply(value.get()), offset, widthFactor
-                )
-        );
-        line.endXProperty().bind(
-                Bindings.createDoubleBinding(
-                        () -> scaleWidth.apply(value.get()), offset, widthFactor
-                )
-        );
-        line.startYProperty().bind(
-                Bindings.createDoubleBinding(
-                        () -> scaleWidth.apply(scaleHeight.apply(FACTOR_BOXPLOT_START)), offset, widthFactor
-                )
-        );
-        line.endYProperty().bind(
-                Bindings.createDoubleBinding(
-                        () -> scaleWidth.apply(scaleHeight.apply(FACTOR_BOXPLOT_END)), offset, widthFactor
-                )
-        );
+        line.startXProperty().bind(scaleWidthBinding.apply(value));
+        line.endXProperty().bind(scaleWidthBinding.apply(value));
+        line.startYProperty().bind(scaleHeightBinding.apply(FACTOR_BOXPLOT_START));
+        line.endYProperty().bind(scaleHeightBinding.apply(FACTOR_BOXPLOT_END));
     }
 
     private void setupBindings() {
@@ -261,26 +226,49 @@ public class BoxPlotSkin<T> extends SkinBase<BoxPlotControl> {
                 )
         );
 
+        // Range
+        range.startXProperty().bind(scaleWidthBinding.apply(boxPlot.lowerWhiskerProperty()));
+        range.endXProperty().bind(scaleWidthBinding.apply(boxPlot.upperWhiskerProperty()));
+        range.startYProperty().bind(scaleHeightBinding.apply(FACTOR_BOXPLOT_END/2));
+        range.endYProperty().bind(scaleHeightBinding.apply(FACTOR_BOXPLOT_END/2));
+
+        // Quartiles
+        quartiles.xProperty().bind(scaleWidthBinding.apply(boxPlot.q1Property()));
+        quartiles.yProperty().bind(scaleHeightBinding.apply(FACTOR_BOXPLOT_START));
+        quartiles.widthProperty().bind(
+                Bindings.createDoubleBinding(
+                        () -> (boxPlot.getQ3() - boxPlot.getQ1()) * widthFactor.get(),
+                        widthFactor, boxPlot.q3Property(), boxPlot.q1Property()
+                )
+        );
+        quartiles.heightProperty().bind(scaleHeightBinding.apply(FACTOR_BOXPLOT_END));
+
+        // all vertical lines
+        setupVerticalLineBindings(lowerWhiskerLine, boxPlot.lowerWhiskerProperty());
+        setupVerticalLineBindings(upperWhiskerLine, boxPlot.upperWhiskerProperty());
+        setupVerticalLineBindings(medianLine, boxPlot.medianProperty());
+//        setupVerticalLineBindings(currentObjectLine, currentObject, height);
+
         // ---- Scale below -----------------------------
-        double translateY = 15;
-        scale.startXProperty().bind(boxPlot.minProperty().subtract(offset).multiply(widthFactor));
-        scale.startYProperty().bind(height.subtract(translateY));
-        scale.endXProperty().bind(boxPlot.maxProperty().subtract(offset).multiply(widthFactor));
-        scale.endYProperty().bind(height.subtract(translateY));
-        dataScale.startXProperty().bind(boxPlot.minProperty().subtract(offset).multiply(widthFactor));
-        dataScale.startYProperty().bind(height.subtract(TRANSLATE_Y * FACTOR_BOXPLOT_END));
-        dataScale.endXProperty().bind(boxPlot.maxProperty().subtract(offset).multiply(widthFactor));
-        dataScale.endYProperty().bind(height.subtract(TRANSLATE_Y * FACTOR_BOXPLOT_END));
+        scale.startXProperty().bind(scaleWidthBinding.apply(boxPlot.minProperty()));
+        scale.endXProperty().bind(scaleWidthBinding.apply(boxPlot.maxProperty()));
+        scale.startYProperty().bind(scaleHeightBinding.apply(FACTOR_SCALE_END));
+        scale.endYProperty().bind(scaleHeightBinding.apply(FACTOR_SCALE_END));
+
+        dataScale.startXProperty().bind(scaleWidthBinding.apply(boxPlot.minProperty()));
+        dataScale.endXProperty().bind(scaleWidthBinding.apply(boxPlot.maxProperty()));
+        dataScale.startYProperty().bind(scaleHeightBinding.apply(FACTOR_DATA_SCALE_END));
+        dataScale.endYProperty().bind(scaleHeightBinding.apply(FACTOR_DATA_SCALE_END));
 
         scaleLeft.startXProperty().set(0);
-        scaleLeft.startYProperty().bind(height.subtract(TRANSLATE_Y * FACTOR_BOXPLOT_END));
         scaleLeft.endXProperty().bind(dataScale.startXProperty());
-        scaleLeft.endYProperty().bind(height.subtract(TRANSLATE_Y * FACTOR_BOXPLOT_END));
+        scaleLeft.startYProperty().bind(scaleHeightBinding.apply(FACTOR_DATA_SCALE_END));
+        scaleLeft.endYProperty().bind(scaleHeightBinding.apply(FACTOR_DATA_SCALE_END));
 
         scaleRight.startXProperty().bind(dataScale.endXProperty());
-        scaleRight.startYProperty().bind(height.subtract(TRANSLATE_Y * FACTOR_BOXPLOT_END));
         scaleRight.endXProperty().bind(width);
-        scaleRight.endYProperty().bind(height.subtract(TRANSLATE_Y * FACTOR_BOXPLOT_END));
+        scaleRight.startYProperty().bind(scaleHeightBinding.apply(FACTOR_DATA_SCALE_END));
+        scaleRight.endYProperty().bind(scaleHeightBinding.apply(FACTOR_DATA_SCALE_END));
 
         drawLabel(minimum, boxPlot.minProperty());
         drawLabel(maximum, boxPlot.maxProperty());
@@ -291,10 +279,15 @@ public class BoxPlotSkin<T> extends SkinBase<BoxPlotControl> {
         drawLabel(medianLabel, boxPlot.medianProperty());
     }
 
-    private void drawLabel(Label label, ReadOnlyDoubleProperty element) {
-        label.textProperty().bind(element.asString("%." + DECIMALS + "f"));
-        label.translateXProperty().bind(element.subtract(offset).multiply(widthFactor).subtract(label.widthProperty().divide(2)));
-        label.translateYProperty().bind(height.subtract(15));
+    private void drawLabel(Label label, ReadOnlyDoubleProperty value) {
+        label.textProperty().bind(value.asString(LABEL_FORMATTING));
+        label.translateXProperty().bind(
+                Bindings.createDoubleBinding(
+                        () -> scaleWidth.apply(value.get()) - (label.widthProperty().get() / 2),
+                        offset, widthFactor, value, label.widthProperty()
+                )
+        );
+        label.translateYProperty().bind(scaleHeightBinding.apply(FACTOR_DATA_SCALE_END));
     }
 
     private void drawOutlier(T element, double value) {
