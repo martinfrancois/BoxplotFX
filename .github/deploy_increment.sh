e03579365865c7ce0793833c50ff108a1ec7effa
martinfrancois <fracoisma@hotmail.com>
2019-07-27 22:52:55 +0200

    use TAG_BRANCH instead of TRAVIS_BRANCH
    TRAVIS_BRANCH equals the tag name in case of a tag that's pushed, so we need to get the name of the branch first

--- .github/deploy_increment.sh
+++ .github/deploy_increment.sh
@@ -1,21 +1,19 @@
 #!/bin/bash
 
-if [[ "$TRAVIS_PULL_REQUEST" == "true" || ("$TRAVIS_BRANCH" != "master" && "$TRAVIS_BRANCH" != "master-11") && "$TRAVIS_TAG" == "" ]]
+if [[ ("$TRAVIS_PULL_REQUEST" == "true" || ("$TAG_BRANCH" != "master" && "$TAG_BRANCH" != "master-11")) && "$TRAVIS_TAG" == "" ]]
 then
   echo "No tag was made from master or master-11, skipping incrementing version."
   exit 0
 fi
 
-rev=$(git rev-parse --short HEAD)
-
-CHANGELOG_EMAIL=${CHANGELOG_EMAIL:='build@travis-ci.org'}
+INCREMENT_EMAIL=${INCREMENT_EMAIL:='build@travis-ci.org'}
 
 git config user.name "Travis CI"
-git config user.email $CHANGELOG_EMAIL
+git config user.email $INCREMENT_EMAIL
 
 git remote add upstream "https://$GITHUB_TOKEN@github.com/martinfrancois/BoxplotFX.git"
 git fetch upstream
-git checkout $TRAVIS_BRANCH
+git checkout $TAG_BRANCH
 
 # increment version
 mvn versions:set -DnewVersion=$TRAVIS_TAG -DoldVersion=* -DgroupId=* -DartifactId=*
@@ -23,4 +21,4 @@ mvn versions:set -DnewVersion=$TRAVIS_TAG -DoldVersion=* -DgroupId=* -DartifactI
 mvn versions:update-child-modules
 
 git commit -am "increment version to ${TRAVIS_TAG}"
-git push upstream $TRAVIS_BRANCH
+git push upstream $TAG_BRANCH
