d6d1508726b19407d88cca69d0b7291ccf7c0f19
martinfrancois <fracoisma@hotmail.com>
2019-07-28 01:57:20 +0200

    use $TRAVIS_REPO_SLUG wherever possible

--- .github/deploy_prepare.sh
+++ .github/deploy_prepare.sh
@@ -22,7 +22,7 @@ git config user.name "Travis CI"
 git config user.email "build@travis-ci.org"
 
 echo "Prepare Git for Commits"
-git remote add upstream "https://$GITHUB_TOKEN@github.com/martinfrancois/BoxplotFX.git"
+git remote add upstream "https://$GITHUB_TOKEN@github.com/$TRAVIS_REPO_SLUG.git"
 git fetch upstream
 git checkout $TAG_BRANCH
 echo "Finished Prepare Git for Commits"
