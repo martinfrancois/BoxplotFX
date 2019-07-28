30f96fd0bdd56f8cf70e94f0e85ad658f13d2ccc
martinfrancois <fracoisma@hotmail.com>
2019-07-28 12:29:41 +0200

    further parametrize scripts

--- .github/deploy_bintray_central.sh
+++ .github/deploy_bintray_central.sh
@@ -4,7 +4,7 @@ cp .github/.travis.settings.xml $HOME/.m2/settings.xml
 echo "Deploy to Bintray"
 mvn deploy -DskipTests
 echo "Sync to Maven Central"
-result=$(curl -X POST -u $BINTRAY_USER:$BINTRAY_API_KEY https://api.bintray.com/maven_central_sync/martinfrancois/BoxplotFX/BoxplotFX/versions/$TRAVIS_TAG)
+result=$(curl -X POST -u $BINTRAY_USER:$BINTRAY_API_KEY https://api.bintray.com/maven_central_sync/$BINTRAY_SUBJECT/$BINTRAY_REPO/$BINTRAY_PACKAGE/versions/$TRAVIS_TAG)
 if [[ $result ==  *"Successfully synced and closed repo."* ]]
 then
 	echo Successfully synced to Maven Central
